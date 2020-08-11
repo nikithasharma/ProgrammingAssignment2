565b204afa227af56f99eeb1d1d5dca807dc8c98
Nikitha <pnikithasharma@yahoo.com>
2020-08-11 19:50:29 +0530

    After writing programs to compute Inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 by Nikitha 
 
-## Write a short comment describing this function
+## The function makeCacheMatrix, takes a matrix x and creates a matrix that can
+## cache it's inverse. 
+## The code was simple enough to write using the example of cache mean, and editing 
+## mean to inverse and changing some of the variable names.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+      x<<-y
+      inv<<-NULL
+  }
+  get<-function()x
+  setinv<-function(solve)inv<<-solve
+  getinv<-function()inv
+  list(set=set,get=get,
+       setinv=setinv,
+       getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve, takes a matrix x and returns the inverse of the matrix.
+## It checks if there's cached inverse and if there is, it prints out "Using cached data". 
+## If there isn't, it prints out "Computing inverse for the first time"
+## The code was simple enough to write using the example of cache mean, and editing 
+## mean to inverse and changing some of the variable names.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()
+  if(!is.null(inv)){
+      message("Using cached data")
+      return(inv)
+  }
+  message("Computing Inverse for the first time")
+  data<-x$get()
+  inv<-solve(data, ...)
+  x$setinv(inv)
+  inv
 }
